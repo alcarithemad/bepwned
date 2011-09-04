d48e1de80f8d5cc857cc77619a0f867f18e99c4a
Colin Valliant <alcarithemad@gmail.com>
2011-09-03 20:07:23 -0700

    commiting before switching to kryptn's color analysis method

---
+++ click.py
@@ -0,0 +1,64 @@
+import ctypes
+from PIL import ImageGrab
+
+class Rect(ctypes.Structure):
+	_fields_ = [('left', ctypes.c_long),
+				('top', ctypes.c_long),
+				('right', ctypes.c_long),
+				('bottom', ctypes.c_long)
+	]
+	def packed_coords(self):
+		return (self.left, self.top, self.right, self.bottom)
+
+user32 = ctypes.windll.user32
+
+HWND = user32.FindWindowA(None, "Bejeweled 3")
+
+bbox = Rect() # this variable is IMPORTANT
+user32.GetWindowRect(HWND, ctypes.byref(bbox))
+
+def get_screen():
+	return ImageGrab.grab(bbox.packed_coords())
+
+class MouseInput(ctypes.Structure): #this is actually an INPUT struct with a MOUSEINPUT where the union should be
+	_fields_ = [('type', ctypes.c_ulong), #this should always be 0
+				('dx', ctypes.c_long),
+				('dy', ctypes.c_long),
+				('mouseData', ctypes.c_ulong),
+				('dwFlags', ctypes.c_ulong),
+				('time', ctypes.c_ulong),
+				('dwExtraInfo', ctypes.POINTER(ctypes.c_ulong))
+	]
+
+ScreenWidth = 1280
+ScreenHeight = 1024
+
+def send_click(x, y):
+	clickarray = (MouseInput * 3)()
+	clickarray[0] = MouseInput()
+	clickarray[0].dx = x*(65335/ScreenWidth)
+	clickarray[0].dy = y*(65335/ScreenHeight)
+	clickarray[0].dwFlags = 0x0001 | 0x8000 # MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE
+	clickarray[1] = MouseInput()
+	clickarray[1].dx = x*(65335/ScreenWidth)
+	clickarray[1].dy = y*(65335/ScreenHeight)
+	clickarray[1].dwFlags = 0x0002 # MOUSEEVENTF_LEFTDOWN
+	clickarray[2] = MouseInput()
+	clickarray[2].dx = x*(65335/ScreenWidth)
+	clickarray[2].dy = y*(65335/ScreenHeight)
+	clickarray[2].dwFlags = 0x0004 # MOUSEEVENTF_LEFTUP
+
+	user32.SendInput(3, clickarray, ctypes.sizeof(MouseInput))
+
+grid_offset = (350, 90)
+grid_square = 85
+def swap_gems(gem1, gem2):
+	print 'clicking at', gem1, gem2
+	gem1 = map(lambda x:x*grid_square, gem1)
+	gem2 = map(lambda x:x*grid_square, gem2)
+	gem1[0] += grid_offset[0]+bbox.left
+	gem1[1] += grid_offset[1]+bbox.top
+	gem2[0] += grid_offset[0]+bbox.left
+	gem2[1] += grid_offset[1]+bbox.top
+	send_click(*gem1)
+	send_click(*gem2)
--- play.py
+++ play.py
@@ -1,76 +1,94 @@
-import ctypes
-from PIL import ImageGrab
+from itertools import dropwhile
+import click
 
-class Rect(ctypes.Structure):
-	_fields_ = [('left', ctypes.c_long),
-				('top', ctypes.c_long),
-				('right', ctypes.c_long),
-				('bottom', ctypes.c_long)
-	]
-	def packed_coords(self):
-		return ((self.left, self.top), (self.right, self.bottom))
+class point(object):
+	def __init__(self, a, b):
+		self.a, self.b = a, b
+	def __add__(self, p):
+		return point(self.a+p.a, self.b+p.b)
+	def tuple(self):
+		return (self.a, self.b)
 
-user32 = ctypes.windll.user32
-HWND = user32.FindWindowA(None, "Bejeweled 3")
-user32.SetForegroundWindow(HWND)
-bbox = Rect() # this variable is IMPORTANT
-user32.GetWindowRect(HWND, ctypes.byref(bbox))
+class Grid(object):
+	gleft = click.grid_offset[0]
+	gtop = click.grid_offset[1]
+	gright = gleft+(click.grid_square*8)
+	gbottom = gtop+(click.grid_square*8)
+	def __init__(self, imagefunc):
+		self.grid = [[0]*8 for x in xrange(8)]
+		self.imagefunc = imagefunc
 
-def get_screen():
-	return ImageGrab.grab(bbox.packed_coords())
+	def __getitem__(self, p):
+		print self.grid[p.a][p.b]
+		return self.grid[p.a][p.b]
+	
+	def updategrid(self):
+		img = self.imagefunc()
+		img.crop((self.gleft, self.gtop, self.gright, self.gbottom))
+		img = img.load()
+		for x in xrange(len(self.grid)):
+			for y in xrange(len(self.grid[0])):
+				points = [(x*click.grid_square+a, y*click.grid_square+b) for b in xrange(82) for a in xrange(82)]
+				colors = map(lambda p: img[p[0], p[1]], points)
+				avgcolor = map(lambda x:x/len(points), map(sum, zip(*colors)))
+				#print avgcolor
+				if (x,y) in ((2,6), (2, 4)):
+					print avgcolor
+				self.grid[x][y] = avgcolor
+	
+	def search(self, func):
+		func(self)
 
-class MouseInput(ctypes.Structure): #this is actually an INPUT struct with a MOUSEINPUT where the union should be
-	_fields_ = [('type', ctypes.c_ulong), #this should always be 0
-				('dx', ctypes.c_long),
-				('dy', ctypes.c_long),
-				('mouseData', ctypes.c_ulong),
-				('dwFlags', ctypes.c_ulong),
-				('time', ctypes.c_ulong),
-				('dwExtraInfo', ctypes.POINTER(ctypes.c_ulong))
-	]
-
-ScreenWidth = 1280
-ScreenHeight = 1024
-
-def send_click(x, y):
-	clickarray = (MouseInput * 3)()
-	clickarray[0] = MouseInput()
-	clickarray[0].dx = x*(65335/ScreenWidth)
-	clickarray[0].dy = y*(65335/ScreenHeight)
-	clickarray[0].dwFlags = 0x0001 | 0x8000 # MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE
-	clickarray[1] = MouseInput()
-	clickarray[1].dx = x*(65335/ScreenWidth)
-	clickarray[1].dy = y*(65335/ScreenHeight)
-	clickarray[1].dwFlags = 0x0002 # MOUSEEVENTF_LEFTDOWN
-	clickarray[2] = MouseInput()
-	clickarray[2].dx = x*(65335/ScreenWidth)
-	clickarray[2].dy = y*(65335/ScreenHeight)
-	clickarray[2].dwFlags = 0x0004 # MOUSEEVENTF_LEFTUP
-
-	user32.SendInput(3, clickarray, ctypes.sizeof(MouseInput))
-
-grid_offset = (350, 90)
-grid_square = 85
-def swap_gems(gem1, gem2):
-	print 'clicking at', gem1, gem2
-	gem1 = map(lambda x:x*grid_square, gem1)
-	gem2 = map(lambda x:x*grid_square, gem2)
-	gem1[0] += grid_offset[0]+bbox.left
-	gem1[1] += grid_offset[1]+bbox.top
-	gem2[0] += grid_offset[0]+bbox.left
-	gem2[1] += grid_offset[1]+bbox.top
-	send_click(*gem1)
-	send_click(*gem2)
+tplates = [(
+			(point(1, 0), point(0, 1), point(1, 2)),	# if these are all the same,
+			(point(0, 1), point(1, 1)),					# swap these
+			(6, 4)	# edge
+			),
+			(
+			(point(0, 0), point(0, 2), point(0, 3)),	# if these are all the same,
+			(point(0, 0), point(0, 1)),					# swap these
+			(7, 4)	# edge
+			),
+			(
+			(point(0, 0), point(1, 0), point(3, 0)),	# if these are all the same,
+			(point(3, 0), point(2, 0)),					# swap these
+			(4, 7)	# edge
+			),
+			(
+			(point(1, 0), point(2, 0), point(0, 1)),	# if these are all the same,
+			(point(0, 0), point(0, 1)),					# swap these
+			(4, 6)	# edge
+			)
+		]
+def template(tplate):
+	t = tplate[0]
+	swap = tplate[1]
+	edge = tplate[2]
+	def f(grid):
+		for x in xrange(edge[0]+1):
+			for y in xrange(edge[1]+1):
+				g = point(x, y)
+				points = [grid[g+p] for p in t]
+				if len(list(dropwhile(lambda x: x==grid[g+t[0]], points))) > 1:
+					click.swap_gems((swap[0]+g).tuple(), (swap[1]+g).tuple())
+	return f
+					
 
 if __name__ == '__main__':
-	user32.BringWindowToTop(HWND)
-	user32.SetForegroundWindow(HWND)
-	import random
-	for x in xrange(100):
-		first = random.randint(1, 6), random.randint(1, 6)
-		if random.random() > 0.5:
-			second = first[0]+random.choice((-1, 1)), first[1]
-		else:
-			second = first[0], first[1]+random.choice((-1, 1))
-		swap_gems(first, second)
+	click.user32.BringWindowToTop(click.HWND)
+	click.user32.SetForegroundWindow(click.HWND)
+	g = Grid(click.get_screen)
+	g.updategrid()
+	for t in tplates:
+		f = template(t)
+		g.search(f)
+	print 'ps', (g[point(0, 4)], g[point(0, 6)], g[point(0, 7)])
+	# import random
+	# for x in xrange(2):
+	# 	first = random.randint(1, 6), random.randint(1, 6)
+	# 	if random.random() > 0.5:
+	# 		second = first[0]+random.choice((-1, 1)), first[1]
+	# 	else:
+	# 		second = first[0], first[1]+random.choice((-1, 1))
+	# 	click.swap_gems(first, second)
 	#swap_gems((4, 3), (5, 3))
